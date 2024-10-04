5f00d7226619bfa1e7c000f045d79d91782bb8d0
Jeff Verkoeyen <jeff@clutch.engineering>
2024-10-04 13:41:02 -0700

    Add IONIQ 6.

--- Sources/pages/SupportedCars/SupportMatrix.swift
+++ Sources/pages/SupportedCars/SupportMatrix.swift
@@ -78,6 +78,11 @@ let makes: [Make: [Model: [VehicleSupportStatus]]] = [
       .init(years: 2024...2024, testingStatus: .activeTester("zachmiles", id: 76), stateOfCharge: .obd, stateOfHealth: .unk, charging: .unk, cells: .unk, fuelLevel: .na, speed: .unk, range: .unk, odometer: .unk, tirePressure: .unk),
       .testerNeeded(years: 2025...2025)
     ],
+    "IONIQ 6": [
+      .testerNeeded(years: 2023...2023),
+      .init(years: 2024...2024, testingStatus: .activeTester("Danmartyn", id: 98), stateOfCharge: .obd, stateOfHealth: .unk, charging: .unk, cells: .unk, fuelLevel: .na, speed: .unk, range: .unk, odometer: .unk, tirePressure: .unk),
+      .testerNeeded(years: 2025...2025)
+    ],
   ],
   "Kia": [
     " EV 6": [
