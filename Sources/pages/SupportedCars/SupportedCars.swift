e4894284ed30452c0b54b234372e6069c56ab9f3
Jeff Verkoeyen <jeff@clutch.engineering>
2024-10-16 21:00:32 -0700

    Add beta tester link to every make.

--- Sources/pages/SupportedCars/SupportedCars.swift
+++ Sources/pages/SupportedCars/SupportedCars.swift
@@ -241,6 +241,41 @@ struct MakeSupportSection: View {
         MakeCard(make: make)
           .margin(.bottom, 16)
 
+        VStack(alignment: .center, spacing: 8) {
+          Link(becomeBetaURL) {
+            VStack(alignment: .center, spacing: 4) {
+              H1("Don't see your car?")
+                .fontSize(.large)
+                .fontSize(.extraLarge, condition: .desktop)
+                .bold()
+                .fontDesign("rounded")
+              Text("Become a Sidecar beta tester, get \(betaSubscriptionLength) months free")
+                .fontSize(.small)
+                .fontSize(.base, condition: .desktop)
+                .fontWeight(.medium)
+                .fontDesign("rounded")
+              Text("Learn more")
+                .fontWeight(.bold)
+                .fontDesign("rounded")
+                .fontSize(.large)
+                .underline(condition: .hover)
+            }
+            .textAlignment(.center)
+            .classNames(["bg-gradient-to-tl", "from-cyan-500", "to-blue-600"])
+            .transition(.all)
+            .textColor(.white)
+            .padding(.horizontal, 16)
+            .padding(.vertical, 12)
+            .background(.zinc, darkness: 100)
+            .background(.zinc, darkness: 900, condition: .dark)
+            .cornerRadius(.extraExtraLarge)
+          }
+        }
+        .frame(width: 0.8)
+        .frame(width: 0.6, condition: .desktop)
+        .margin(.horizontal, .auto)
+        .margin(.bottom, 16)
+
         VStack(alignment: .leading, spacing: 16) {
           for (model, statuses) in models.sorted(by: { modelNameForSorting($0.key) < modelNameForSorting($1.key) }) {
             ModelSupportSection(model: model, statuses: statuses)
@@ -467,7 +502,7 @@ struct SupportedCars: View {
                   .bold()
                   .fontDesign("rounded")
                 Text("Become a Sidecar beta tester, get \(betaSubscriptionLength) months free")
-                  .fontSize(.large, condition: .desktop)
+                  .fontSize(.large)
                   .fontSize(.extraLarge, condition: .desktop)
                   .fontWeight(.medium)
                   .fontDesign("rounded")
@@ -475,6 +510,7 @@ struct SupportedCars: View {
                   .fontWeight(.bold)
                   .fontDesign("rounded")
                   .fontSize(.large)
+                  .underline(condition: .hover)
               }
               .textAlignment(.center)
               .classNames(["bg-gradient-to-tl", "from-cyan-500", "to-blue-600"])
