d866015ca5327c2302f04c7efbb3c624e35fafcf
Jeff Verkoeyen <jeff@clutch.engineering>
2024-10-24 23:23:35 -0700

    Add santa fe hybrid symbol.

--- Sources/pages/SupportedCars/SupportMatrix.swift
+++ Sources/pages/SupportedCars/SupportMatrix.swift
@@ -93,7 +93,7 @@ let makes: [Make: [Model: [VehicleSupportStatus]]] = [
       .init(years: 2023...2023, testingStatus: .activeTester("Briantran33"), stateOfCharge: .obd, stateOfHealth: .unk, charging: .unk, cells: .unk, fuelLevel: .na, speed: .unk, range: .unk, odometer: .unk, tirePressure: .unk),
       .testerNeeded(years: 2024...2025)
     ],
-    " Sante Fe Hybrid": [
+    "Sante Fe Hybrid": [
       .testerNeeded(years: 2021...2021),
       .init(years: 2022...2022, testingStatus: .activeTester("funtaine"), stateOfCharge: .obd, stateOfHealth: .unk, charging: .obd, cells: .unk, fuelLevel: .obd, speed: .obd, range: .unk, odometer: .obd, tirePressure: .unk),
       .testerNeeded(years: 2023...2025)
