604d4169d4449170b3502f7d9817e71d05acdc43
Jeff Verkoeyen <jeff@clutch.engineering>
2024-11-04 07:20:20 -0800

    Add accord.

--- Sources/pages/SupportedCars/SupportMatrix.swift
+++ Sources/pages/SupportedCars/SupportMatrix.swift
@@ -440,7 +440,7 @@ let makes: [Make: [Model: [VehicleSupportStatus]]] = [
     ],
   ],
   "Honda": [
-    " Accord": [
+    "Accord": [
       .testerNeeded(years: 1996...2012),
       .init(years: 2013...2013, testingStatus: .activeTester("elijah"), stateOfCharge: .na, stateOfHealth: .na, charging: .na, cells: .na, fuelLevel: .obd, speed: .obd, range: .unk, odometer: .unk, tirePressure: .unk),
       .testerNeeded(years: 2014...2024),
