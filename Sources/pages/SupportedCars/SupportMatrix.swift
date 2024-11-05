5a719bd177d58e3a094801b6ae2fb511631fa778
Jeff Verkoeyen <jeff@clutch.engineering>
2024-11-05 08:58:53 -0800

    Onboard.

--- Sources/pages/SupportedCars/SupportMatrix.swift
+++ Sources/pages/SupportedCars/SupportMatrix.swift
@@ -1215,7 +1215,7 @@ let makes: [Make: [Model: [VehicleSupportStatus]]] = [
     ],
     "bZ4X": [
       .testerNeeded(years: 2023...2023),
-      .init(years: 2024...2024, testingStatus: .activeTester("Jefbos90"), stateOfCharge: .obd, stateOfHealth: .unk, charging: .unk, cells: .unk, fuelLevel: .unk, speed: .obd, range: .unk, odometer: .obd, tirePressure: .unk),
+      .init(years: 2024...2024, testingStatus: .onboarded, stateOfCharge: .obd, stateOfHealth: .unk, charging: .unk, cells: .unk, fuelLevel: .unk, speed: .obd, range: .unk, odometer: .obd, tirePressure: .unk),
       .testerNeeded(years: 2025...2025)
     ],
   ],
