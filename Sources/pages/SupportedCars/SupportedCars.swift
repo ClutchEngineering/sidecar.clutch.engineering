cb33834fc20ccd3adceb6811480c199d0d8b209a
Jeff Verkoeyen <jeff@clutch.engineering>
2024-11-10 12:17:38 -0800

    Add more testers.

--- Sources/pages/SupportedCars/SupportedCars.swift
+++ Sources/pages/SupportedCars/SupportedCars.swift
@@ -204,12 +204,38 @@ struct VehicleSupportStatus {
   let odometer: SupportState
   let tirePressure: SupportState
 
+  init(years: ClosedRange<Int>, testingStatus: TestingStatus, stateOfCharge: SupportState, stateOfHealth: SupportState, charging: SupportState, cells: SupportState, fuelLevel: SupportState, speed: SupportState, range: SupportState, odometer: SupportState, tirePressure: SupportState) {
+    self.years = years
+    self.testingStatus = testingStatus
+    self.stateOfCharge = stateOfCharge
+    self.stateOfHealth = stateOfHealth
+    self.charging = charging
+    self.cells = cells
+    self.fuelLevel = fuelLevel
+    self.speed = speed
+    self.range = range
+    self.odometer = odometer
+    self.tirePressure = tirePressure
+  }
+
+  init(years: Int, testingStatus: TestingStatus, stateOfCharge: SupportState, stateOfHealth: SupportState, charging: SupportState, cells: SupportState, fuelLevel: SupportState, speed: SupportState, range: SupportState, odometer: SupportState, tirePressure: SupportState) {
+    self.init(years: years...years, testingStatus: testingStatus, stateOfCharge: stateOfCharge, stateOfHealth: stateOfHealth, charging: charging, cells: cells, fuelLevel: fuelLevel, speed: speed, range: range, odometer: odometer, tirePressure: tirePressure)
+  }
+
   static func testerNeeded(years: ClosedRange<Int>) -> Self {
     .init(years: years, testingStatus: .testerNeeded, stateOfCharge: .unk, stateOfHealth: .unk, charging: .unk, cells: .unk, fuelLevel: .unk, speed: .unk, range: .unk, odometer: .unk, tirePressure: .unk)
   }
 
+  static func testerNeeded(years: Int) -> Self {
+    .testerNeeded(years: years...years)
+  }
+
   static func newTester(years: ClosedRange<Int>, username: String) -> Self {
     .init(years: years, testingStatus: .activeTester(username), stateOfCharge: .unk, stateOfHealth: .unk, charging: .unk, cells: .unk, fuelLevel: .unk, speed: .unk, range: .unk, odometer: .unk, tirePressure: .unk)
+  }
+
+  static func newTester(years: Int, username: String) -> Self {
+    .newTester(years: years...years, username: username)
   }
 }
 
