1fb8b817d6e3ff57d21ddd84d1fbeea45a5e4abc
Jeff Verkoeyen <jeff@clutch.engineering>
2024-11-10 12:03:50 -0800

    Add Tucson.

--- Sources/pages/SupportedCars/SupportMatrix.swift
+++ Sources/pages/SupportedCars/SupportMatrix.swift
@@ -553,7 +553,7 @@ let makes: [Make: [Model: [VehicleSupportStatus]]] = [
     " Sonata": [
       .init(years: 1996...2024, testingStatus: .partiallyOnboarded, stateOfCharge: .obd, stateOfHealth: .unk, charging: .unk, cells: .unk, fuelLevel: .obd, speed: .obd, range: .unk, odometer: .obd, tirePressure: .unk),
     ],
-    " Tucson": [
+    "Tucson": [
       .testerNeeded(years: 2004...2018),
       .init(years: 2019...2019, testingStatus: .activeTester("BradP1011"), stateOfCharge: .na, stateOfHealth: .na, charging: .na, cells: .na, fuelLevel: .obd, speed: .obd, range: .unk, odometer: .unk, tirePressure: .unk),
       .testerNeeded(years: 2020...2025),
