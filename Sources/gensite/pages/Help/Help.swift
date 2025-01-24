2818e7f16c57dd9f0d1df65c85f59ea45335084e
Jeff Verkoeyen <jeff@clutch.engineering>
2025-01-24 12:53:33 -0800

    Add docs on exporting specific scan sessions.

--- Sources/gensite/pages/Help/Help.swift
+++ Sources/gensite/pages/Help/Help.swift
@@ -58,6 +58,18 @@ log is stored privately on your phone.
 To export the logs from your phone, follow these steps from within the Sidecar app:
 
 1. Tap the Settings tab.
+2. Scroll down and tap the "Scan sessions" button.
+3. Tap the three-dot menu button.
+4. Tap "Export latest sessions" or "Select sessions" and then select the specific sessions you want to export.
+5. A share sheet will appear once you've selected your sessions for export. You can now airdrop the file, send it via email, or perform any other share action with the data. You'll typically want to email this file to support@clutch.engineering for onboarding purposes.
+
+Note: your OBD scan logs will typically contain your vehicle identification number (VIN).
+
+![100%](/gfx/help/export-specific-scan-sessions.png)
+
+You can also export your entire scan session database by following these steps:
+
+1. Tap the Settings tab.
 2. Scroll down and tap the "Storage" button.
 3. Tap "Scanning sessions".
 4. A share sheet will appear. You can now airdrop the file, send it via email, or perform any other share action with the data.
