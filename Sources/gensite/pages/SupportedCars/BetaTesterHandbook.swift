9ba5ba4006d22ecf046bd843e4142e50a60de8aa
Jeff Verkoeyen <jeff@clutch.engineering>
2025-03-31 20:22:24 -0700

    Update the beta handbook

--- Sources/gensite/pages/SupportedCars/BetaTesterHandbook.swift
+++ Sources/gensite/pages/SupportedCars/BetaTesterHandbook.swift
@@ -54,9 +54,7 @@ To become a Sidecar beta tester, you’ll need:
 
 2.  **iOS 18+ device**: Testing should ideally be done on your primary iPhone. Android support isn’t available yet, but if you’re interested, email Jeff at support@clutch.engineering to express interest.
 
-3.  **Willingness to share feedback**: You'll be asked to provide feedback regularly via Sidecar's companion forum, [cars.forum](https://meta.cars.forum/viewforum.php?f=12).
-
-4.  **OBD data sharing**: If you're testing OBD support, you will need to share scan logs and screenshots to help with data accuracy.
+3.  **OBD data sharing**: If you're testing OBD support, you will need to share scan logs and screenshots to help with data accuracy.
 
 Note: if you do not have an OBD scanner yet but would like to purchase one, a set of tested scanners
 is available in the [OBD-II Scanning guide](/scanning/).
@@ -76,20 +74,18 @@ Conditions
 How to Register
 ---------------
 
-To become a beta tester, you must first create an account on [cars.forum](https://cars.forum), the community forum for Sidecar and other Clutch Engineering products. If you don’t have an account yet, you can [register here](https://meta.cars.forum/ucp.php?mode=register). It's free.
-
-You must then register your interest by filling in the form below:
+Please register your interest by filling in the form below:
 
 <iframe class="airtable-embed" src="https://airtable.com/embed/appqtcCtdikk37lX9/pagNEk4vaHhLGgfwl/form" frameborder="0" onmousewheel="" width="100%" height="700" style="background: transparent; border: 1px solid #ccc;"></iframe>
 
-You will receive your subscription promo code after you have:
+We're onboarding cars in the order shown in [the leaderboard](http://sidecar.clutch.engineering/leaderboard/).
+               
+Once we start onboarding your model, you will receive your subscription promo code after you have:
 
-1. been added to the beta tester group on cars.forum,
-2. [installed Sidecar](https://apps.apple.com/us/app/electric-sidecar/id1663683832) and connected
+1. [installed Sidecar](https://apps.apple.com/us/app/electric-sidecar/id1663683832) and connected
 it to your car either with a Connected Account or via [OBD-II](https://sidecar.clutch.engineering/scanning/)
 (you can connect your car without a subscription),
-3. started or contributed to a thread in the [vehicle onboarding forum](https://meta.cars.forum/viewforum.php?f=12), and
-3. if you are testing OBD logs, you've emailed an [export of your OBD logs](https://sidecar.clutch.engineering/help/#how-to-export-obd-scan-logs) to support@clutch.engineering.
+3. emailed an [export of your OBD logs](https://sidecar.clutch.engineering/help/#how-to-export-obd-scan-logs) to support@clutch.engineering.
 """)
       .padding(.bottom, 16)
 
