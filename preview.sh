0c12f5b25a72ca5953f2733e208576318b02a680
Jeff Verkoeyen <jeff@clutch.engineering>
2025-05-09 16:36:40 -0700

    Remove the preview script.

--- preview.sh
+++
@@ -1,1 +0,0 @@
-(trap 'kill 0' SIGINT; while sleep 0.1; do find Sources/gensite -type f -name "*.md" -o -name "*.swift" | entr -d swift run gensite; done & python3 -m http.server -d site/ 8090 & npx tailwindcss -i tailwind.css -o ./site/css/main.css --watch)
