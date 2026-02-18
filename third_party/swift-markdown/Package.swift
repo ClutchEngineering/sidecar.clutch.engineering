8b9d44b8e849105852ab52780670b32da5974e67
Jeff Verkoeyen <jeff@clutch.engineering>
2026-02-18 11:08:42 -0500

    More local.

--- third_party/swift-markdown/Package.swift
+++ third_party/swift-markdown/Package.swift
@@ -43,24 +43,7 @@ let package = Package(
     swiftLanguageModes: [.v5]
 )
 
-// If the `SWIFTCI_USE_LOCAL_DEPS` environment variable is set,
-// we're building in the Swift.org CI system alongside other projects in the Swift toolchain and
-// we can depend on local versions of our dependencies instead of fetching them remotely.
-if ProcessInfo.processInfo.environment["SWIFTCI_USE_LOCAL_DEPS"] == nil {
-    // Building standalone, so fetch all dependencies remotely.
-    package.dependencies += [
-        .package(url: "https://github.com/swiftlang/swift-cmark.git", branch: "gfm"),
-    ]
-    
-    // SwiftPM command plugins are only supported by Swift version 5.6 and later.
-    #if swift(>=5.6)
-    package.dependencies += [
-        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.1.0"),
-    ]
-    #endif
-} else {
-    // Building in the Swift.org CI system, so rely on local versions of dependencies.
-    package.dependencies += [
-        .package(path: "../cmark"),
-    ]
-}
+// Building in the Swift.org CI system, so rely on local versions of dependencies.
+package.dependencies += [
+    .package(path: "../swift-cmark"),
+]
